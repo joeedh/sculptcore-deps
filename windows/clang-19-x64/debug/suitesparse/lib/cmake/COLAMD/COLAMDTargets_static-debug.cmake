3f3e7fb82d64f1ea9f13c7af357562deb8c8108a
Joe Eagar <joeedh@gmail.com>
2026-06-12 10:03:41 -0700

    update deps

---
+++ windows/clang-19-x64/debug/suitesparse/lib/cmake/COLAMD/COLAMDTargets_static-debug.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Debug".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "SuiteSparse::COLAMD_static" for configuration "Debug"
+set_property(TARGET SuiteSparse::COLAMD_static APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(SuiteSparse::COLAMD_static PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "C"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/colamd_static.lib"
+  )
+
+list(APPEND _cmake_import_check_targets SuiteSparse::COLAMD_static )
+list(APPEND _cmake_import_check_files_for_SuiteSparse::COLAMD_static "${_IMPORT_PREFIX}/lib/colamd_static.lib" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
