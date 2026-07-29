bfaf1da31693152106f69291a99e67c367f767f7
Joe Eagar <joeedh@gmail.com>
2026-07-28 23:26:58 -0700

    add linux/clang-18-x64/relwithdebinfo

---
+++ linux/clang-18-x64/relwithdebinfo/suitesparse/lib/cmake/CCOLAMD/CCOLAMDTargets_static-relwithdebinfo.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "RelWithDebInfo".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "SuiteSparse::CCOLAMD_static" for configuration "RelWithDebInfo"
+set_property(TARGET SuiteSparse::CCOLAMD_static APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(SuiteSparse::CCOLAMD_static PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELWITHDEBINFO "C"
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/libccolamd.a"
+  )
+
+list(APPEND _cmake_import_check_targets SuiteSparse::CCOLAMD_static )
+list(APPEND _cmake_import_check_files_for_SuiteSparse::CCOLAMD_static "${_IMPORT_PREFIX}/lib/libccolamd.a" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
